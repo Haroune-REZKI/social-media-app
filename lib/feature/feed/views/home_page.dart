84242bd075331188fa24cd8ce94566f59434f07f
Mohcen <hugoboy2311@gmail.com>
2023-11-18 11:34:20 +0100

    configured project

---
+++ lib/feature/feed/views/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeFeed extends StatelessWidget {
+  const HomeFeed({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Hello world")),
+    );
+  }
+}
